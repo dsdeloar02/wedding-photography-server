32194f1f4517cdf40bbc0bd9a6d4c3cd85610b16
dsdeloar02 <dsdeloar02@gmail.com>
2022-11-09 13:56:43 +0600

    update opetation added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
@@ -25,16 +25,39 @@ async function run() {
         const serviceCollection = client.db('photoGraphar').collection('service');
         app.get('/homeservices', async(req, res) => {
             const query = {};
-            const sort = {time : 1};
+            // const sort = ObjectId.getTimestamp();
             // console.log(sort);
-            const cursor = serviceCollection.find(query).sort(sort);
-            const service = await cursor.toArray();
+            const cursor = serviceCollection.find(query).sort({_id : -1 });
+            const service = await cursor.limit(3).toArray();
             res.send(service);
         })
         app.post('/service', async(req, res) => {
             const service = req.body;            
             console.log(service);
             const result = await serviceCollection.insertOne(service);
+            res.send(result)
+        })
+        app.get('/homeservices/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id:ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        app.put('/homeservices/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id : ObjectId(id)};
+            const service = req.body;
+            console.log(service)
+            const option = {upsert : true};
+            const updatedUser = {
+                $set: {
+                    package_name : service.package_name,
+                    thumbnail : service.thumbnail,
+                    price : service.price,
+                    description : service.description
+                }
+            }
+            const result = await serviceCollection.updateOne(filter, updatedUser, option);
             res.send(result)
         })
     }
